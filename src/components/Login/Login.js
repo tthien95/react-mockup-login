92b0814a5303348a3a66a2c6e93b09579d50da1a
Thien Trinh <thien.trinh@hcl.com>
2022-05-24 03:00:24 +0700

    useEffect with clean up function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const timeOutHandler = setTimeout(() => {
+      console.log('Check Form Validity!!');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('CLEAN UP!!');
+      clearTimeout(timeOutHandler);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
